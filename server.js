ae8041eacc6fddbf54bd6e79b6d3820f02543466
banifest <banifest@gmail.com>
2017-09-26 10:52:13 +0300

    added logger system

--- server.js
+++ server.js
@@ -1,43 +1,57 @@
 const net = require('net');
+const fs = require("fs");
 const port = 8124;
+
+function writeLog(str, client) {
+    fs.appendFileSync(`${client.id.toString()}.log`, str.toString() + '\n\r')
+}
 
 const server = net.createServer((client) => {
     console.log('Client connected');
     client.setEncoding('utf8');
 
-    client.id = Date.now();
-
     client.on('data', (data) => {
         console.log(data);
         if(!client.ACK)
         {
+            client.id = Date.now();
+            writeLog(`id = ${client.id.toString()}`, client);
             if(data === 'QA')
             {
                 client.write('ACK');
                 client.ACK = true;
+
+                writeLog(`ACK`, client);
             }
             else
             {
                 client.write('DEC');
                 client.ACK = false;
+
+                writeLog(`DEC`, client);
             }
         }
         else
         {
+            writeLog(`Вопрос - ${data.toString()}`, client);
             if(Math.random()>0.5)
             {
-                console.log('Да');
+                writeLog(`ответ - да`, client);
                 client.write('да');
             }
             else
             {
-                console.log('Нет');
+                writeLog(`ответ - нет`, client);
                 client.write('нет');
             }
         }
     });
 
-    client.on('end', () => console.log('Client disconnected'));
+    client.on('end', () =>
+    {
+        console.log('Client disconnected')
+        writeLog(`Client disconnected`, client);
+    });
 });
 
 server.listen(port, () => {
